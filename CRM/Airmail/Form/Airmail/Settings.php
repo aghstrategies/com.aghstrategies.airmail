1ebd53214d30bfb503ec83e6f5ab5ad26bf9a5ce
Alice Frumin <alice@aghstrategies.com>
2017-10-23 16:19:52 -0400

    adding form for settings

---
+++ CRM/Airmail/Form/Airmail/Settings.php
@@ -0,0 +1,78 @@
+<?php
+
+use CRM_Airmail_ExtensionUtil as E;
+
+/**
+ * Form controller class
+ *
+ * @see https://wiki.civicrm.org/confluence/display/CRMDOC/QuickForm+Reference
+ */
+class CRM_Airmail_Form_Airmail_Settings extends CRM_Core_Form {
+  public function buildQuickForm() {
+
+    // add form elements
+    $this->add(
+      'select', // field type
+      'favorite_color', // field name
+      'Favorite Color', // field label
+      $this->getColorOptions(), // list of options
+      TRUE // is required
+    );
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => E::ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    // export form elements
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+
+  public function postProcess() {
+    $values = $this->exportValues();
+    $options = $this->getColorOptions();
+    CRM_Core_Session::setStatus(E::ts('You picked color "%1"', array(
+      1 => $options[$values['favorite_color']],
+    )));
+    parent::postProcess();
+  }
+
+  public function getColorOptions() {
+    $options = array(
+      '' => E::ts('- select -'),
+      '#f00' => E::ts('Red'),
+      '#0f0' => E::ts('Green'),
+      '#00f' => E::ts('Blue'),
+      '#f0f' => E::ts('Purple'),
+    );
+    foreach (array('1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e') as $f) {
+      $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', array(1 => $f));
+    }
+    return $options;
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  public function getRenderableElementNames() {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+}
