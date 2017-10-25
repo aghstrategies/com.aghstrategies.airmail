5a9636b25b2f69c4a0629c7049d9deb583911501
Alice Frumin <alice@aghstrategies.com>
2017-10-25 13:04:48 -0400

    cleaning up

--- CRM/Airmail/Page/Webhook.php
+++ CRM/Airmail/Page/Webhook.php
@@ -4,15 +4,14 @@ use CRM_Airmail_ExtensionUtil as E;
 class CRM_Airmail_Page_Webhook extends CRM_Core_Page {
 
   public function run() {
-
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    // get information sent from Amazon SNS
     $events = json_decode(file_get_contents('php://input'));
-    //CRM_Core_Error::debug_log_message(print_r(getallheaders(), TRUE), FALSE, 'AirmailWebhook');
-    CRM_Core_Error::debug_log_message(print_r($events->Type, TRUE), FALSE, 'AirmailWebhook');
-    //CRM_Core_Error::debug_log_message(print_r($_POST, TRUE), FALSE, 'AirmailWebhook');
+    // NOTE if you want to log the contents of the post un comment this line
+    //  CRM_Core_Error::debug_log_message('sns' . print_r($events, TRUE), FALSE, 'AirmailWebhook');
     if ($events->Type == 'SubscriptionConfirmation' && !empty($events->SubscribeURL)) {
-     $snsResponse == file_get_contents($events->SubscribeURL);
-     CRM_Core_Error::debug_log_message('sns' . print_r($snsResponse, TRUE), FALSE, 'AirmailWebhook');
+      // Go to the subscribe URL to confirm end point
+      // TODO parse the xml and save the info to civi just in case
+      $snsResponse == file_get_contents($events->SubscribeURL);
     }
     // Example: Assign a variable for use in a template
     $this->assign('currentTime', date('Y-m-d H:i:s'));
