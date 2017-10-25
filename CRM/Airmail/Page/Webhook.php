c40a1ffac006d2bd45f17adff0d007c353f8649b
AGH Strategies <info@aghstrategies.com>
2017-10-25 16:49:27 +0000

    dealing with subscribe url

--- CRM/Airmail/Page/Webhook.php
+++ CRM/Airmail/Page/Webhook.php
@@ -4,13 +4,19 @@ use CRM_Airmail_ExtensionUtil as E;
 class CRM_Airmail_Page_Webhook extends CRM_Core_Page {
 
   public function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(E::ts('Webhook'));
 
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    $events = json_decode(file_get_contents('php://input'));
+    //CRM_Core_Error::debug_log_message(print_r(getallheaders(), TRUE), FALSE, 'AirmailWebhook');
+    CRM_Core_Error::debug_log_message(print_r($events->Type, TRUE), FALSE, 'AirmailWebhook');
+    //CRM_Core_Error::debug_log_message(print_r($_POST, TRUE), FALSE, 'AirmailWebhook');
+    if ($events->Type == 'SubscriptionConfirmation' && !empty($events->SubscribeURL)) {
+     $snsResponse == file_get_contents($events->SubscribeURL);
+     CRM_Core_Error::debug_log_message('sns' . print_r($snsResponse, TRUE), FALSE, 'AirmailWebhook');
+    }
     // Example: Assign a variable for use in a template
     $this->assign('currentTime', date('Y-m-d H:i:s'));
-
-    parent::run();
+    CRM_Utils_System::civiExit();
   }
 
 }
