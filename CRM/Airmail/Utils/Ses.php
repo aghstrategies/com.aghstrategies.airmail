354800dbe29f82b3068c3602bd5be2e8e8693f3b
Alice Frumin <alice@aghstrategies.com>
2017-10-26 18:51:48 -0400

    response message

--- CRM/Airmail/Utils/Ses.php
+++ CRM/Airmail/Utils/Ses.php
@@ -15,34 +15,32 @@ class CRM_Airmail_Utils_Ses extends CRM_Airmail_Utils {
     }
     CRM_Core_Error::debug_log_message('getNotifcations ses', FALSE, 'AirmailWebhook');
     // If the message is a notification of a mailing event
-    if ($events->Type == 'Notification' && $events->Message) {
+    if ($events->Type == 'Notification') {
       $responseMessage = json_decode($events->Message);
-      CRM_Core_Error::debug_log_message($responseMessage, FALSE, 'AirmailWebhook');
-      CRM_Core_Error::debug_log_message(print_r($responseMessage, TRUE), FALSE, 'AirmailWebhook');
-
       self::processNotification($responseMessage->mail->source, $responseMessage->notificationType, $responseMessage);
     }
-
   }
 
-  public function processNotification($source, $type, $extra) {
-    $mailingJobInfo = self::parseSourceString($source);
-    if (!empty($type) && !empty($mailingJobInfo) && $mailingJobInfo['job_id'] > 0) {
-      switch ($type) {
-        // NOTE there are other Event Types including "Reject", "Send", "Delivery", "Click", "Open", and "Rendering Failure" which we are not currently addressing
-        case 'Bounce':
-          $body = "Bounce Description: {$extra->bounce->bounceType} {$extra->bounce->bounceSubType}";
-          self::bounce($mailingJobInfo['job_id'], $mailingJobInfo['event_queue_id'], $mailingJobInfo['hash'], $body);
-          break;
+  public function processNotification($source = NULL, $type = NULL, $extra = NULL) {
+    if ($source) {
+      $mailingJobInfo = self::parseSourceString($source);
+      if (!empty($type) && !empty($mailingJobInfo) && !empty($mailingJobInfo['job_id'])) {
+        switch ($type) {
+          // NOTE there are other Event Types including "Reject", "Send", "Delivery", "Click", "Open", and "Rendering Failure" which we are not currently addressing
+          case 'Bounce':
+            $body = "Bounce Description: {$extra->bounce->bounceType} {$extra->bounce->bounceSubType}";
+            self::bounce($mailingJobInfo['job_id'], $mailingJobInfo['event_queue_id'], $mailingJobInfo['hash'], $body);
+            break;
 
-        case 'Complaint':
-          // TODO opt out contact entirely
-          self::spamreport($mailingJobInfo['job_id'], $mailingJobInfo['event_queue_id'], $mailingJobInfo['hash']);
-          break;
+          case 'Complaint':
+            // TODO opt out contact entirely
+            self::spamreport($mailingJobInfo['job_id'], $mailingJobInfo['event_queue_id'], $mailingJobInfo['hash']);
+            break;
 
-        default:
-          # code...
-          break;
+          default:
+            # code...
+            break;
+        }
       }
     }
   }
