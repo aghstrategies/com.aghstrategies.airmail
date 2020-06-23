ed38ed57944e3fe0063eb030cbd744ae67c286ab
sushantpaste <sushantpaste@gmail.com>
2020-06-23 17:05:54 +0530

    Added bounce report in CiviCRM

--- CRM/Airmail/Backend/Elastic.php
+++ CRM/Airmail/Backend/Elastic.php
@@ -14,7 +14,8 @@ class CRM_Airmail_Backend_Elastic implements CRM_Airmail_Backend {
 
   public function processMessages($event) {
     $status = $event['status'];
-    $mailingJobInfo = E::parseSourceString($event['from']);
+    //Parse postback url to get all required mailing information.
+    $mailingJobInfo = E::parseSourceString($event['postback']);
     $params = [
       'job_id' => $mailingJobInfo['job_id'],
       'event_queue_id' => $mailingJobInfo['event_queue_id'],
@@ -25,6 +26,8 @@ class CRM_Airmail_Backend_Elastic implements CRM_Airmail_Backend {
       case 'Bounce / Error':
       case 'Bounce':
       case 'Error':
+        // Add bounce report in CiviCRM.
+        CRM_Airmail_EventAction::bounce($params);
         CRM_Core_Error::debug_log_message("Bounce Error:\n" . print_r($event, TRUE));
         break;
     }
@@ -39,7 +42,11 @@ class CRM_Airmail_Backend_Elastic implements CRM_Airmail_Backend {
    *   The mailing context.
    */
   public function alterMailParams(&$params, $context) {
-
+    // Add custom headers for ElasticEmail
+    // This is required so we will get postback in Elastic email response.
+    if ($context != 'messageTemplate') {
+      if (!array_key_exists('headers', $params)) $params['headers'] = array();
+      $params['headers']['X-ElasticEmail-Postback'] = $params['Return-Path'];
+    }
   }
-
 }
