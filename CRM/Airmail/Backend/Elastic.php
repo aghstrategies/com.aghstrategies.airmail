c5476d5666ba637e28e0aa28e677da2931ee3796
Surya server <surya@n.ganivet.net>
2020-06-26 12:05:24 +0000

    Fixes for adding bounce type and bounce resoan in CiviCRM

--- CRM/Airmail/Backend/Elastic.php
+++ CRM/Airmail/Backend/Elastic.php
@@ -16,10 +16,14 @@ class CRM_Airmail_Backend_Elastic implements CRM_Airmail_Backend {
     $status = $event['status'];
     //Parse postback url to get all required mailing information.
     $mailingJobInfo = E::parseSourceString($event['postback']);
+    //Map Elastic email bounce types and resoan with CiviCRM.
+    $bounce_details = self::getBounceTypeMessages($event);
     $params = [
       'job_id' => $mailingJobInfo['job_id'],
       'event_queue_id' => $mailingJobInfo['event_queue_id'],
       'hash' => $mailingJobInfo['hash'],
+      'bounce_type_id' => $bounce_details['bounce_type_id'],
+      'bounce_reason' => $bounce_details['bounce_reason'],
     ];
     switch ($status) {
       // When you want to receive notifications for bounced emails.
@@ -48,5 +52,58 @@ class CRM_Airmail_Backend_Elastic implements CRM_Airmail_Backend {
       if (!array_key_exists('headers', $params)) $params['headers'] = array();
       $params['headers']['X-ElasticEmail-Postback'] = $params['Return-Path'];
     }
+  }
+
+ /**
+  *
+  * Function for getting bounce type and message.
+  */
+ public function getBounceTypeMessages($event) {
+  //Elastic Email bounce categories.
+  $elastic_categories = array(
+    'Away' => array('Unknown'),
+    'Relay' => array('Throttled', 'GreyListed', 'Timeout'),
+    'Invalid' => array('NoMailbox', 'NotDelivered', 'DNSProblem', 'AccountProblem', 'SPFProblem'),
+    'Spam' => array('ContentFilter', 'Spam', 'Blacklisted', 'ConnectionTerminated', 'ConnectionProblem'),
+    'Abuse' => array('AbuseReport'),
+  );
+
+  //Default bounce types from civicrm.
+  $civicrm_bounces = array(
+    'Away' => 2,    // soft, retry 30 times
+    'Relay' => 9,   // soft, retry 3 times
+    'Invalid' => 6, // hard, retry 1 time
+    'Spam' => 10,   // hard, retry 1 time
+    'Abuse' => 10,  // hard, retry 1 time
+  );
+  foreach ($elastic_categories as $value => $categories) {
+    if (in_array($event['category'], $categories)){
+      $bounce_type_id = $civicrm_bounces[$value];
+    }
+  }
+  // As we don't have any resaon in Elastic Email response.
+  // Mapping of the bounce resaon with Elastic Email short messages. Refer: https://help.elasticemail.com/en/articles/2300650-what-are-the-bounce-error-categories-and-filters
+  $elastic_mail_messgaes = array(
+    'Unknown' => 'Unknown Error',
+    'Throttled' => 'The recipient server did not accept the email within 48 hours',
+    'GreyListed' => 'The email was not delivered because the recipient server has determined that this email has not been seen in the configuration provide',
+    'Timeout' => 'The email was not delivered because a timeout occurred',
+    'NoMailbox' => 'The email address does not appear to exist',
+    'NotDelivered' => 'The recipient has a blocked status for either hard bouncing, being unsubscribed or complained',
+    'DNSProblem' => 'The domain part of the address does not exist',
+    'AccountProblem' => 'There is something wrong with the mailbox of the recipient, eg. the mailbox is full and cannot accept more emails',
+    'SPFProblem' => 'The email was not delivered because there was an issue validating the SPF record for the domain of this email',
+    'ContentFilter' => 'Unknown Error',
+    'Spam' => 'The email was rejected because it matched a profile the internet community has labeled as Spam',
+    'Blacklisted' => 'Email is black listed',
+    'ConnectionTerminated' => 'The status of the email is not known for sure because the recipient server terminated the connection without returning a message code or status',
+    'ConnectionProblem' => 'The email was not delivered because of a connection problem',
+    'AbuseReport' => 'Unknown Error',
+  );
+  $bounce_reason = $elastic_mail_messgaes[$event['category']];
+  return array(
+           'bounce_type_id' =>  $bounce_type_id,
+           'bounce_reason' => $bounce_reason,
+         );
   }
 }
